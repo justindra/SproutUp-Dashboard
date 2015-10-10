4690a3330adcec8f442c5b5305022f07b52d21c6
Justin Rahardjo <justindra91@gmail.com>
2015-10-10 19:14:07 +1000

    Added plantlist and subscriptions

--- client/templates/pages/plants/plants.js
+++ client/templates/pages/plants/plants.js
@@ -2,3 +2,9 @@ Template.plantList.created = function () {
     var instance = this;
     this.subscribe('plants');
 };
+
+Template.plantList.helpers({
+    plants: function () {
+        return Plants.find({userId: Meteor.userId()});
+    }
+});
