4efdca881bff98c5b21793385a45481ef4e4bb59
Justin Rahardjo <justindra91@gmail.com>
2015-10-10 18:00:21 +1000

    Adde bootstrap

--- server/startup/database-seed.js
+++ server/startup/database-seed.js
@@ -6,4 +6,15 @@ Meteor.startup(function () {
         });
         Meteor.call('insertDataPoint', plantId, 20);
     }
+
+    if(Meteor.users.find().count() < 1) {
+        Accounts.createUser({
+            username: 'joeschmoe',
+            password: 'password',
+            email: 'joe@schmoe.com',
+            profile: {
+                name: 'Joe Schmoe'
+            }
+        });
+    }
 });
